2054301bd5d2a6badc4a6ec7379082ded34eb130
fwesrd <2895746239@qq.com>
2026-03-18 20:35:07 +0800

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,62 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 pngr.rc 使用
+//
+#define IDR_MENU_MAIN                   40000
+#define ID_MENU_LEAD_OUT                40001
+#define ID_MENU_LEAD_IN                 40002
+#define ID_MENU_CONTROL                 40003
+#define IDD_CONTROLDIALOG               50000
+#define IDC_BODY_LIST                   50001
+#define IDC_BODY_ADD                    50002
+#define IDC_BODY_DELETE                 50003
+#define IDC_BODY_CLEAR                  50004
+#define IDC_FOLLOW                      50005
+#define IDC_FOCUS_BODY                  50006
+#define IDC_FOCUS_KERR                  50007
+#define IDC_UNFOLLOW                    50008
+#define IDC_UNFOCUS                     50009
+#define IDC_TIME_RATE                   50010
+#define IDC_PARTICLE_COUNT              50011
+#define IDC_PARTICLE_DISTANCE           50012
+#define IDC_PARTICLE_OFFSETX            50013
+#define IDC_PARTICLE_OFFSETY            50014
+#define IDC_PARTICLE_OFFSETZ            50015
+#define IDC_KERR_HAS                    50016
+#define IDC_DISK_HAS                    50017
+#define IDC_KERR_MASS                   50018
+#define IDC_KERR_A_RATE                 50019
+#define IDC_KERR_X                      50020
+#define IDC_KERR_Z                      50021
+#define IDC_KERR_Y                      50022
+#define IDC_KERR_SPEED                  50023
+#define IDC_SKYBOX                      50024
+#define IDC_SKYBOX_FILE                 50025
+#define IDC_SKYBOX_RANDOM               50026
+#define IDC_SKYBOX_DENSITY              50027
+#define IDC_LIGHT                       50028
+#define IDC_LIGHT_STEP_COUNT            50029
+#define IDC_LIGHT_STEP_MIN              50030
+#define IDC_LIGHT_STEP_MAX              50031
+#define IDC_SCALE_TIME                  50032
+#define IDC_SCALE_SPACE                 50033
+#define IDC_FOV                         50034
+#define IDC_SPEED_MOVE                  50035
+#define IDC_SPEED_SPIN                  50036
+#define IDC_POSITION_X                  50037
+#define IDC_POSITION_Y                  50038
+#define IDC_POSITION_Z                  50039
+#define IDC_APPLY                       60001
+#define IDC_RUN                         60002
+#define IDC_STOP                        60003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        60000
+#define _APS_NEXT_COMMAND_VALUE         60000
+#define _APS_NEXT_CONTROL_VALUE         60002
+#define _APS_NEXT_SYMED_VALUE           60000
+#endif
+#endif
